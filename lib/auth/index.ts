364bdfb5460d0276fe41d7aaa98a204bd88c3232
Donato Jr <donato.junior.07@gmail.com>
2023-01-08 20:27:14 -0300

    feat(auth): move auth logic to new file

---
+++ lib/auth/index.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import config from '../config/spotify'
+import { SpotifyRefreshToken } from '../types/spotify'
+
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
+
+const basicAuthBuffer = Buffer.from(
+  `${config.client_id}:${config.client_secret}`
+).toString('base64')
+
+export default async function getNewToken() {
+  const options = new URLSearchParams({
+    grant_type: 'refresh_token',
+    refresh_token: config.refresh_token as string,
+  })
+
+  axios
+
+  const { data } = await axios<SpotifyRefreshToken>(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basicAuthBuffer}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: options,
+  })
+
+  return data
+}
