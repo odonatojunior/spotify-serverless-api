39d4c772f708a3e7538f6539c88737303aaacf81
Donato Jr <donato.junior.07@gmail.com>
2023-01-08 20:31:54 -0300

    feat(api): types and 'limit' query parameter

--- api/index.ts
+++ api/index.ts
@@ -1,63 +1,42 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { SpotifyResponse, SpotifyTrack } from 'lib/types/spotify'
-
-const client_id = process.env.SPOTIFY_CLIENT_ID
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET
-const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
-
-const basicAuthBuffer = Buffer.from(`${client_id}:${client_secret}`).toString(
-  'base64'
-)
+import {
+  CustomSpotifyApiResponse,
+  SpotifyItem,
+  SpotifyResponse,
+} from '../lib/types/spotify'
+import getNewToken from '../lib/auth'
 
 const RECENTLY_PLAYED_ENDPOINT =
   'https://api.spotify.com/v1/me/player/recently-played'
-const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
 
 export default async function (
   request: VercelRequest,
   response: VercelResponse
 ) {
   const { access_token } = await getNewToken()
-
-  const { data } = await axios(RECENTLY_PLAYED_ENDPOINT, {
+  const axiosConfig: AxiosRequestConfig = {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
     params: {
-      limit: 5,
+      limit: request.query?.limit || 5,
     },
-  })
+  }
 
-  // gambiarra da desgraça pra parsear o json rapaz
-  //@ts-ignore
-  const albums = data.items.map(({ track }) => {
+  const { data } = await axios<SpotifyResponse<SpotifyItem>>(
+    RECENTLY_PLAYED_ENDPOINT,
+    axiosConfig
+  )
+
+  const albums = data.items.map(({ track }): CustomSpotifyApiResponse => {
     return {
       name: track.name,
+      artists: track.artists.map((artist) => artist.name),
       album: track.album.name,
-      cover: track.album.images[0].url,
-      artist: track.artists[0].name,
+      cover: track.album.images,
     }
   })
 
-  response.status(200).json(data)
-}
-
-async function getNewToken() {
-  //@ts-ignore
-  const options = new URLSearchParams({
-    grant_type: 'refresh_token',
-    refresh_token,
-  })
-
-  const { data } = await axios(TOKEN_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${basicAuthBuffer}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    data: options,
-  })
-
-  return data
+  response.status(200).json(albums)
 }
